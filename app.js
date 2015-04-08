50d75d7088d1dbdfa91ac4a225bfb0f6813abe8e
Merritt Lawrenson <flamingveggies@users.noreply.github.com>
2015-04-07 22:26:51 -0700

    port

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var router = require("./app/router.js");
+var port = process.env.PORT || 3000;
 
 var http = require('http');
 http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
   router.css(request, response);
-}).listen(80);
+}).listen(port);
 console.log('The server is running! You\'re listening at port 3000.');
