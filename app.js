5c824e0677a4a64cba1ffb8a0e3477f49d95afa8
Merritt Lawrenson <flamingveggies@users.noreply.github.com>
2015-04-07 22:14:47 -0700

    port

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
   router.css(request, response);
-}).listen();
+}).listen(80);
 console.log('The server is running! You\'re listening at port 3000.');
